2437f16eb9e3c2451b389c7b57eea00f652f3d2d
satyajit sahoo <55840143+satyajit501@users.noreply.github.com>
2023-02-24 02:09:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.header("hello world")
+st.header("hello guys just i made this first webapp by the help of Innomatics Research Labs and little bit of google. so now lets connect on linkedin --- https://www.linkedin.com/in/satyajit-sahoo01/")
